361fb6e090b806a948b1a7c429522d2462e8e535
NicoBernardes <nicolasbernardes84@gmail.com>
2025-10-16 13:34:44 -0300

    Handler_error added

--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -28,9 +28,9 @@ func (r *repositoryMock) Save(campaign *Campaign) error {
 	return args.Error(0)
 }
 
-func (r *repositoryMock) Get() []Campaign {
+func (r *repositoryMock) Get() ([]Campaign, error) {
 	//args := r.Called(campaign)
-	return nil
+	return nil, nil
 }
 func Test_Create_Campaign(t *testing.T) {
 	assert := assert.New(t)
